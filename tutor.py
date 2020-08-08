cd74ba61c70cfe365e22388427376fc1fd516896
Ashish Rao <ashish.arartc@gmail.com>
2020-08-07 17:58:34 -0700

    detect user state in solution

--- tutor.py
+++ tutor.py
@@ -17,6 +17,32 @@ class Tutor(object):
         self.soln_sep = soln.split('.')
         self.sentence_embeddings = self.model.encode(self.soln_sep)
 
+    def get_similarity(self, a, b):
+        return abs(1 - spatial.distance.cosine(a, b))
+
+    def get_user_state(self, user_state):
+        # First find the step of soln closest to what user typed
+        curr_embedding = self.model.encode(user_state)
+
+        min_dist = None
+        min_idx = 0
+
+        for i in range(len(self.sentence_embeddings)):
+            sent_embedding = self.sentence_embeddings[i]
+            sim = self.get_similarity(curr_embedding, sent_embedding)
+
+            if min_dist is None or sim > min_dist:
+                min_dist = sim
+                min_idx = i
+
+        return min_idx
+
+
 if __name__ == '__main__':
     soln = open("solution.txt").read()
     tutor = Tutor(soln)
+
+    user_state = "I know it's a geometric series, so the ratio should be log_2(x)/log_4(x). But I'm not sure what to do next"
+    idx = tutor.get_user_state(user_state)
+    print(idx)
+    print(tutor.soln_sep[idx])
